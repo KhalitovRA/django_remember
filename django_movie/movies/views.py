862c195627d8853c4b8f3f2704d22041569f13f0
KhalitovRA <halitov-02@mail.ru>
2022-07-26 18:32:56 +0300

    create reviews

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
+from .forms import ReviewForm
 from .models import Movie
 
 
@@ -15,3 +16,15 @@ class MovieDetailView(DetailView):
 
     model = Movie
     slug_field = 'url'
+
+
+class AddReview(View):
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
