d97bfeead128b4073143b4edccaee5055c1108d1
KhalitovRA <halitov-02@mail.ru>
2022-08-11 19:58:45 +0300

    drf configs

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'rest_framework',
 
     'ckeditor_uploader',
     'ckeditor',
@@ -89,8 +90,12 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'remember_django',
+        'USER': 'postgres',
+        'PASSWORD': 'railkha175',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
